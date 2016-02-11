6239669d325712c98d8060262def64badff002af
Wilson <w7leung@ucsd.edu>
2016-02-11 06:14:30 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,7 +27,21 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + projectID, addProject)
+	console.log("project/" + idNumber);
 }
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML ='<img src="' + result['image'] + '" class="detailsImage">' 
+	+ '<p><small>' + result['date'] + '</small>' + result['summary'] + '</p>';
+
+	 var query = "#project" + result['id'] + " .details";
+
+	 $(query).html(projectHTML);
+
+} 	
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +49,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	e.preventDefault();
+	$.get("/palette", randomPalette);
+}
+
+function randomPalette(result){
+	//console.log(result);
+	var colors = result['colors'];
+	colors = colors['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
